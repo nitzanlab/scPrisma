4ac27420642488fb913c41bc776dddbc85e8fbb4
JonathanKarin <69160570+JonathanKarin@users.noreply.github.com>
2022-04-07 14:06:09 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='scPrisma',
+    version='1.0',
+    packages=['scPrisma'],
+    url='',
+    license='',
+    author='jonathankarin',
+    author_email='jonathan.karin@mail.huji.ac.il',
+    description='scPrisma: inference, filtering and enhancement of periodic signals in single-cell data using spectral template matching ',
+    python_requires='>=3',
+    install_requires=[
+        'numpy', 'scanpy', 'pandas', 'pandas', 'scikit-learn', 'scipy', 'sinkhorn_knopp', 'seaborn']
+
+)
