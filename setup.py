2041419f7859dc8fdaafc5ca546e9b0530ffc4e5
Haimasree <haimasree.il@gmail.com>
2022-12-21 11:47:56 +0200

    Full setup.py with cpu gpu options

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
 from setuptools import setup
 
-setup(
-    name='scPrisma',
-    version='1.0',
-    packages=['scPrisma'],
-    url='',
-    license='',
-    author='jonathankarin',
-    author_email='jonathan.karin@mail.huji.ac.il',
-    description='scPrisma: inference, filtering and enhancement of periodic signals in single-cell data using spectral template matching ',
-    python_requires='>=3',
-    install_requires=[
-        'numpy', 'scanpy', 'numba', 'pandas', 'scikit-learn', 'scipy', 'seaborn','matplotlib','sklearn']
+requirements = [
+    "numpy",
+    "scanpy",
+    "numba",
+    "pandas",
+    "scikit-learn",
+    "scipy",
+    "seaborn",
+    "matplotlib",
+    "sklearn",
+]
+cpu = ["numba"]
 
+gpu = ["torch"]
+
+setup(
+    name="scprisma",
+    version="0.0.5",
+    packages=["scprisma"],
+    url="https://github.com/nitzanlab/scPrisma",
+    license="MIT License",
+    author="jonathankarin",
+    author_email="jonathan.karin@mail.huji.ac.il",
+    maintainer="Haimasree Bhattacharya",
+    maintainer_email="haimasree.il@gmail.com",
+    description="scPrisma: inference, filtering and enhancement of periodic signals in single-cell data using spectral template matching ",
+    python_requires=">=3",
+    install_requires=requirements,
+    extras_require={"cpu": cpu, "gpu": gpu},
 )
